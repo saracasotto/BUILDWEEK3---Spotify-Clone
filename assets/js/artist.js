98742ca719d9b8cec39b37e04be43facf5620e87
chri9230 <cm@emmetistore.it>
2024-06-20 21:52:25 +0200

    added artist.js

--- assets/js/artist.js
+++ assets/js/artist.js
@@ -1,4 +1,46 @@
 document.addEventListener("DOMContentLoaded", async()=>{
-    await asideArtist()
-    await albumClick()
+  
+  await asideArtist()
+  await albumClick()
+  displayArtist()
+    
   }) 
+
+  const params = new URLSearchParams(location.search)
+
+let id = params.get('id')
+
+params.set('id', id);
+
+// console.log(params.toString());
+
+function displayArtist() {
+    fetch("https://striveschool-api.herokuapp.com/api/deezer/artist/" + id, {
+
+    })
+        .then(response => {
+            // console.log(response)
+            response.json()
+            .then(data => {
+                console.log(data);
+                
+                let mostraArtista = document.getElementById('artist')
+                mostraArtista.innerHTML = 
+                `
+                        <div class="card col-12">
+                            <img src="${data.picture}" class="card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">${data.name}</h5>
+
+                                <p class="card-text">${data.id}</p>
+
+
+                            </div>
+                        </div>
+                `
+            })
+        })
+        .catch(error => {
+            console.log('Errore nella richiesta fetch:', error);
+        });
+}
