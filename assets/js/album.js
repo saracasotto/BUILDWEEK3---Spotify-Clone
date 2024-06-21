eba1bf8d4f46a827897c29251f2932ffebd9a39e
saracasotto <saracasotto.dev@gmail.com>
2024-06-21 18:42:33 +0100

    js fix

--- assets/js/album.js
+++ assets/js/album.js
@@ -22,13 +22,17 @@ async function getAlbum() {
         document.getElementById("album-cover").src = cover_xl;
         document.getElementById("card-title-album").innerText = title;
         document.getElementById("card-text-artist").innerText = artist.name;
+        document.getElementById("card-text2").innerText = " · " + release_date + " · " + tracks.data.length + " tracks";
+
+        
 
         let songs = "";
         tracks.data.forEach((track, i) => {
             songs += `<tr>
                         <th scope="row">${i + 1}</th>
-                        <td><p class="track-title">${track.title}<p>
-                        <p class="artist-name">${track.artist.name}<p></td>
+                        <td>
+                        <p class="track-title">${track.title}</p>
+                        <p class="artist-name">${track.artist.name}</p></td>
                         <td><audio controls class="audio-controls">
                             <source src="${track.preview}" type="audio/mp3"></audio>
                         </td>
@@ -39,24 +43,6 @@ async function getAlbum() {
         document.getElementById("albumTable").classList.remove("d-none");
         document.getElementById("tracks").innerHTML = songs;
 
-        album = `<div class="card mb-3" style="width:100%;">
-            <div class="row g-0">
-                <div class="col-md-4">
-                    <img src="${cover_xl}" class="img-fluid rounded-start" alt="${title}">
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <p class="card-text"><small class="text-body-secondary">Album</small></p>
-                        <h1 class="card-title">${title}</h1>
-                        <span class="card-text"><small class="text-body-secondary">${artist.name}</small></span>
-                        <span class="card-text"><small class="text-body-secondary">• ${release_date}</small></span>
-                        <span class="card-text"><small class="text-body-secondary">• ${tracks.data.length} tracks</small></span>
-                    </div>
-                </div>
-            </div>
-        </div>`;
-
-        document.getElementById("searchResults").innerHTML = album;
     } catch (error) {
         console.error("Error fetching album data:", error);
     }
